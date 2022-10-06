3685eebf5f782877ea59d7deac459d9ae1b6ce8b
yasinkrcn <23yasin1999@gmail.com>
2022-10-06 22:13:52 +0300

    user entity add

---
+++ lib/features/auth/domain/entities/user_entities.dart
@@ -0,0 +1,15 @@
+class UserEntities {
+  final String id;
+  final String name;
+  final String surname;
+  final String email;
+  final String profilePhotoUrl;
+
+  UserEntities({
+    required this.id,
+    required this.name,
+    required this.surname,
+    required this.email,
+    required this.profilePhotoUrl,
+  });
+}
