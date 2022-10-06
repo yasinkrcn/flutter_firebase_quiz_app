65eaeb0e77582d9a792294ad8b42e57db951fcce
yasinkrcn <23yasin1999@gmail.com>
2022-10-06 22:06:50 +0300

    app border radius

---
+++ lib/core/constants/app_border_radius.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppBorderRadius {
+  static const BorderRadius borderRadiusAll_6px = BorderRadius.all(Radius.circular(6));
+  static const BorderRadius borderRadiusAll_12px = BorderRadius.all(Radius.circular(12));
+  static const BorderRadius borderRadiusAll_14px = BorderRadius.all(Radius.circular(14));
+  static const BorderRadius borderRadiusAll_16px = BorderRadius.all(Radius.circular(16));
+  static const BorderRadius borderRadiusAll_24px = BorderRadius.all(Radius.circular(24));
+}
