4e745d489569523a54047d957ebf9dcacce87ff0
yasinkrcn <23yasin1999@gmail.com>
2022-10-06 22:07:13 +0300

    app text styles

---
+++ lib/core/constants/app_text_style.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppTextStyles {
+//* Medium
+  static TextStyle medium20px = const TextStyle(
+    fontSize: 20,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle medium15px = const TextStyle(
+    fontSize: 15,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle mediumGreyColor14px = const TextStyle(
+    fontSize: 14,
+    color: AppColors.greyColor,
+    fontWeight: FontWeight.w600,
+  );
+
+  //* Regulars
+  static TextStyle regular24px = const TextStyle(
+    fontSize: 24,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle regular20px = const TextStyle(
+    fontSize: 20,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.normal,
+  );
+
+  static TextStyle regular16px = const TextStyle(
+    fontSize: 16,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle regular14px = const TextStyle(
+    fontSize: 14,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle regular15px = const TextStyle(
+    fontSize: 15,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.normal,
+  );
+
+  static TextStyle regular12px = const TextStyle(
+    fontSize: 12,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle regularPrimaryColor12px = const TextStyle(
+    fontSize: 12,
+    color: AppColors.primaryColor,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle regularGreyColor12px = const TextStyle(
+    fontSize: 10,
+    color: AppColors.greyColor,
+    fontWeight: FontWeight.normal,
+  );
+
+//* Bolds
+  static TextStyle bold60px = const TextStyle(
+    fontSize: 60,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle bold30px = const TextStyle(
+    fontSize: 30,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bold24px = const TextStyle(
+    fontSize: 24,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bold18px = const TextStyle(
+    fontSize: 18,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bold10px = const TextStyle(
+    fontSize: 10,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bold20px = const TextStyle(
+    fontSize: 20,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bold16px = const TextStyle(
+    fontSize: 16,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.bold,
+  );
+}
