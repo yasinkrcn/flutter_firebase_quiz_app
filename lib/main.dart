edc0ffb85f1b2a97c086efe057a6f451354bc261
yasinkrcn <23yasin1999@gmail.com>
2022-10-06 22:06:35 +0300

    firebase entegration

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,44 @@
+import 'package:flutter_firebase_quiz_app/features/auth/view/pages/splash_page.dart';
+
+import '/core/init/injection_container.dart' as sl;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_quiz_app/core/constants/main_provider_list.dart';
+import 'package:flutter_firebase_quiz_app/core/keys/global_key.dart';
+import 'package:flutter_firebase_quiz_app/core/utils/route/router.dart';
 import 'package:flutter_firebase_quiz_app/features/auth/view/pages/login_page.dart';
 import 'package:flutter_firebase_quiz_app/firebase_options.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await sl.init();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
-  runApp(const MyApp());
+  );
+  runApp(
+    MultiProvider(
+      providers: MainProviderList.getMainProviderList(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quiz',
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: generateRoute,
+      navigatorKey: GlobalContextKey.instance.globalKey,
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: const SplashPage(),
     );
   }
 }
-
