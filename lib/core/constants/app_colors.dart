c02df2579a242dd3d181b0b25422aa6d1793bde6
yasinkrcn <23yasin1999@gmail.com>
2022-10-06 22:07:02 +0300

    appcolors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color primaryColor = Color(0xFF0081FE);
+  static const Color secondaryColor = Color(0xFF0A142F);
+  static const Color greyColor = Color(0xFFB2B7C6);
+  static const Color greyDarkColor = Color(0xFF687684);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color successColor = Color(0xFF55B938);
+  static const Color errorColor = Color(0xFFFA1313);
+  static const Color lightBlueColor = Color(0xFFCCE6FF);
+  static const Color backgroundColor = Color(0xFFF9F9FF);
+}
