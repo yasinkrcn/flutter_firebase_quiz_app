8513f1d302f2e2591831b1d37bb3974f0a6e094d
yasinkrcn <23yasin1999@gmail.com>
2022-10-07 19:52:40 +0300

    quiz page minus and plus functions done

--- lib/features/quiz/view/pages/quiz_page.dart
+++ lib/features/quiz/view/pages/quiz_page.dart
@@ -3,13 +3,17 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_quiz_app/core/init/injection_container.dart';
 import 'package:flutter_firebase_quiz_app/core/utils/screen_size.dart';
+import 'package:flutter_firebase_quiz_app/features/quiz/data/model/question_model.dart';
 import 'package:flutter_firebase_quiz_app/features/quiz/view/widgets/answer_card_widget.dart';
 import 'package:flutter_firebase_quiz_app/features/quiz/view/widgets/progress_bar.dart';
+import 'package:flutter_firebase_quiz_app/features/quiz/view/widgets/questionSpace.dart';
 import 'package:flutter_firebase_quiz_app/features/quiz/view_model/quiz_provider.dart';
 import 'package:provider/provider.dart';
 
 class QuizPage extends StatefulWidget {
-  const QuizPage({Key? key}) : super(key: key);
+  const QuizPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<QuizPage> createState() => _QuizPageState();
@@ -41,50 +45,61 @@ class _QuizPageState extends State<QuizPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Consumer<QuizProvider>(
-              builder: (context, quizProvider, child) {
-                return Column(
-                  children: [
-                    ProgressBar(
-                      width: ScreenSize().getWidthPercent(1),
-                      totalValue: 110,
-                      value: quizProvider.time,
-                    ),
-                  ],
-                );
-              },
-            ),
-            const Spacer(),
-            Container(
-              height: 200,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  borderRadius: BorderRadius.circular(16)),
-              child: const Center(
-                  child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Text(
-                  'question',
-                  style: TextStyle(fontSize: 18),
+        child: Consumer<QuizProvider>(
+          builder: (context, quizProvider, child) {
+            return Column(
+              children: [
+                ProgressBar(
+                  width: ScreenSize().getWidthPercent(1),
+                  totalValue: 110,
+                  value: quizProvider.time,
                 ),
-              )),
-            ),
-            const Spacer(),
-            SizedBox(
-              height: 273,
-              width: double.infinity,
-              child: ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return const AnswerCard();
-                },
-                itemCount: 4,
-              ),
-            )
-          ],
+                const Spacer(),
+                SizedBox(
+                  height: 200,
+                  child: ListView.builder(
+                      itemCount: 1,
+                      itemBuilder: ((context, index) {
+                        return QuestionSpace(
+                            question: quizProvider
+                                .questions[quizProvider.categoryIndex]);
+                      })),
+                ),
+                Row(
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          quizProvider.categoryIndexMinus();
+                        },
+                        child: Text('minus')),
+                    SizedBox(
+                      width: 100,
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          quizProvider.categoryIndexPlus();
+                        },
+                        child: Text('plus')),
+                  ],
+                ),
+                const Spacer(),
+                SizedBox(
+                  height: 60,
+                  width: double.infinity,
+                  child: ListView.builder(
+                    itemCount: 1,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (BuildContext context, int index) {
+                      return AnswerCard(
+                        question:
+                            quizProvider.questions[quizProvider.categoryIndex],
+                      );
+                    },
+                  ),
+                )
+              ],
+            );
+          },
         ),
       ),
     );
