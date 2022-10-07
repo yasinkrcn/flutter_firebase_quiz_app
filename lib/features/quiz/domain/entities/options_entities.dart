def6cb3432e3280e1f42a55f11fda9abe8d427ca
yasinkrcn <23yasin1999@gmail.com>
2022-10-07 19:51:58 +0300

    options entity

---
+++ lib/features/quiz/domain/entities/options_entities.dart
@@ -0,0 +1,12 @@
+class OptionsEntites {
+  final String optiona;
+  final String optionb;
+  final String optionc;
+  final String optiond;
+
+  OptionsEntites(
+      {required this.optiona,
+      required this.optionb,
+      required this.optionc,
+      required this.optiond});
+}
