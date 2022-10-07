49dc6683d8858ca7fb22550f188bafd996b4fa13
yasinkrcn <23yasin1999@gmail.com>
2022-10-07 19:52:55 +0300

    anwers card done

--- lib/features/quiz/view/widgets/answer_card_widget.dart
+++ lib/features/quiz/view/widgets/answer_card_widget.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_quiz_app/features/quiz/data/model/question_model.dart';
 
 class AnswerCard extends StatelessWidget {
-  const AnswerCard({
+
+  final QuestionModel question;
+   AnswerCard({
     Key? key,
+    required this.question
   }) : super(key: key);
 
   @override
@@ -15,9 +19,9 @@ class AnswerCard extends StatelessWidget {
         width: double.infinity,
         decoration: BoxDecoration(
             color: Colors.grey, borderRadius: BorderRadius.circular(16)),
-        child: const Center(
+        child:  Center(
           child: Text(
-            'answer',
+            question.options.toString(),
             style: TextStyle(
                 color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),
           ),
