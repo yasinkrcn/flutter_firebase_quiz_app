2627e6454edbbee8c61ad02ad1714ec920896c13
yasinkrcn <23yasin1999@gmail.com>
2022-10-07 19:53:42 +0300

    provider index control function done

--- lib/features/quiz/view_model/quiz_provider.dart
+++ lib/features/quiz/view_model/quiz_provider.dart
@@ -1,8 +1,19 @@
 import 'dart:async';
+import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_quiz_app/features/quiz/data/model/question_model.dart';
 
 class QuizProvider extends ChangeNotifier {
+  List<QuestionModel> questions = [];
+
+  // QuizProvider({
+  //   required this.questions
+  // })
+
+  int categoryIndex = 0;
+
   int _time = 18;
 
   int get time => _time;
@@ -13,7 +24,7 @@ class QuizProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void onInit() {
+  void onInit() async {
     Timer.periodic(const Duration(seconds: 1), (timer) {
       if (time == 0) {
         print('clock stop');
@@ -22,5 +33,38 @@ class QuizProvider extends ChangeNotifier {
         time -= 1;
       }
     });
+
+    await fetchQuestions();
+  }
+
+  Future<void> fetchQuestions() async {
+    final questionList = await FirebaseFirestore.instance
+        .collection('category')
+        .doc('math')
+        .collection('questions')
+        // .doc('options')
+        .get();
+
+    final questionDocs = questionList.docs;
+
+    questions = questionDocs.map((value) {
+      return QuestionModel.fromMap(value.data());
+    }).toList();
+  }
+
+  void categoryIndexPlus() {
+    if (categoryIndex < questions.length - 1) {
+      categoryIndex++;
+    }
+
+    notifyListeners();
+  }
+
+  void categoryIndexMinus() {
+    if (categoryIndex > 0) {
+      categoryIndex--;
+    }
+
+    notifyListeners();
   }
 }
