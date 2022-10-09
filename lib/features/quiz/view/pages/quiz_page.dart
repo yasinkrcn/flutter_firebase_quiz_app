eedc6e0399341859d56cd90d256ce77d9c34bf80
yasinkrcn <23yasin1999@gmail.com>
2022-10-09 20:38:56 +0300

    ui changes

--- lib/features/quiz/view/pages/quiz_page.dart
+++ lib/features/quiz/view/pages/quiz_page.dart
@@ -6,7 +6,7 @@ import 'package:flutter_firebase_quiz_app/core/utils/screen_size.dart';
 import 'package:flutter_firebase_quiz_app/features/quiz/data/model/question_model.dart';
 import 'package:flutter_firebase_quiz_app/features/quiz/view/widgets/answer_card_widget.dart';
 import 'package:flutter_firebase_quiz_app/features/quiz/view/widgets/progress_bar.dart';
-import 'package:flutter_firebase_quiz_app/features/quiz/view/widgets/questionSpace.dart';
+import 'package:flutter_firebase_quiz_app/features/quiz/view/widgets/question_space.dart';
 import 'package:flutter_firebase_quiz_app/features/quiz/view_model/quiz_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -23,8 +23,9 @@ class _QuizPageState extends State<QuizPage> {
   @override
   void initState() {
     //İlgili sayfada direkt olarak bir fonksiyonu bi butona basılmaksızın ayağa kaldırmak için init state içerisine koyacağız
-    sl<QuizProvider>().onInit();
+
     super.initState();
+    sl<QuizProvider>().onInit();
   }
 
 // @override
@@ -48,54 +49,47 @@ class _QuizPageState extends State<QuizPage> {
         child: Consumer<QuizProvider>(
           builder: (context, quizProvider, child) {
             return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 ProgressBar(
                   width: ScreenSize().getWidthPercent(1),
                   totalValue: 110,
                   value: quizProvider.time,
                 ),
-                const Spacer(),
-                SizedBox(
-                  height: 200,
-                  child: ListView.builder(
-                      itemCount: 1,
-                      itemBuilder: ((context, index) {
-                        return QuestionSpace(
+                
+                quizProvider.questions.isNotEmpty
+                    ? Center(
+                        child: QuestionSpace(
                             question: quizProvider
-                                .questions[quizProvider.categoryIndex]);
-                      })),
-                ),
-                Row(
-                  children: [
-                    TextButton(
-                        onPressed: () {
-                          quizProvider.categoryIndexMinus();
-                        },
-                        child: Text('minus')),
-                    SizedBox(
-                      width: 100,
-                    ),
-                    TextButton(
-                        onPressed: () {
-                          quizProvider.categoryIndexPlus();
-                        },
-                        child: Text('plus')),
-                  ],
-                ),
-                const Spacer(),
-                SizedBox(
-                  height: 60,
-                  width: double.infinity,
-                  child: ListView.builder(
-                    itemCount: 1,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (BuildContext context, int index) {
-                      return AnswerCard(
+                                .questions[quizProvider.categoryIndex]),
+                      )
+                    : const SizedBox.shrink(),
+                quizProvider.questions.isNotEmpty
+                    ? AnswerCard(
                         question:
                             quizProvider.questions[quizProvider.categoryIndex],
-                      );
-                    },
-                  ),
+                      )
+                    : const SizedBox.shrink(),
+                Row(
+      
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+
+
+                    GestureDetector(
+
+                      onTap: () => quizProvider.categoryIndexMinus(),
+                      
+                      child: const Icon(Icons.arrow_circle_left,size: 60,color: Colors.red,)),
+                       GestureDetector(
+
+                      onTap: () => quizProvider.categoryIndexPlus(),
+                      
+                      child: const Icon(Icons.arrow_circle_right,size: 60,color: Colors.green,)),
+                    
+                  
+                    
+                  ],
                 )
               ],
             );
