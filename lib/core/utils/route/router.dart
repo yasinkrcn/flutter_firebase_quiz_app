3b8b0d2a1167f04186e961d5e97cd1f4197bea7c
yasinkrcn <23yasin1999@gmail.com>
2022-11-04 13:17:46 +0300

    new routes

--- lib/core/utils/route/router.dart
+++ lib/core/utils/route/router.dart
@@ -4,12 +4,14 @@ import 'package:flutter_firebase_quiz_app/features/auth/view/pages/login_page.da
 import 'package:flutter_firebase_quiz_app/features/auth/view/pages/sign_up_page.dart';
 import 'package:flutter_firebase_quiz_app/features/home/view/pages/home_page.dart';
 import 'package:flutter_firebase_quiz_app/features/quiz/view/pages/quiz_page.dart';
+import 'package:flutter_firebase_quiz_app/features/quiz/view/pages/score_page.dart';
 
 class PageRoutes {
   static const String loginPage = "/loginPage";
   static const String signUpPage = "/signUpPage";
   static const String homePage = "/homePage";
   static const String quizPage = "/quizPage";
+  static const String scorePage = "/scorePage";
 }
 
 Route<dynamic> generateRoute(RouteSettings settings) {
@@ -20,8 +22,10 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return CupertinoPageRoute(builder: (_) => SignUpPage());
     case PageRoutes.homePage:
       return CupertinoPageRoute(builder: (_) => const HomePage());
- case PageRoutes.quizPage:
-      return CupertinoPageRoute(builder: (_) =>  QuizPage());
+    case PageRoutes.quizPage:
+      return CupertinoPageRoute(builder: (_) => const QuizPage());
+    case PageRoutes.scorePage:
+      return CupertinoPageRoute(builder: (_) => const ScorePage());
 
     default:
       return CupertinoPageRoute(builder: (_) => const LoginPage());
