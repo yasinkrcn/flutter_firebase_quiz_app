7c09a5cebf5fed2cb254f4fc38519c19d0219e48
yasinkrcn <23yasin1999@gmail.com>
2022-11-11 13:28:11 +0300

    assets imported

--- lib/core/constants/assets_paths.dart
+++ lib/core/constants/assets_paths.dart
@@ -10,5 +10,15 @@ class AssetsPath {
   final String loginSVG = 'assets/svg/login.svg';
   final String googleSVG = 'assets/svg/google.svg';
   final String backgroundSVG = 'assets/svg/background.svg';
+  final String errorSVG = 'assets/svg/error.svg';
+  final String successSVG = 'assets/svg/success.svg';
+  final String warningSVG = 'assets/svg/warning.svg';
 
+  // !Category
+  final String sportSVG = 'assets/svg/sport.svg';
+  final String historySVG = 'assets/svg/history.svg';
+  final String geographySVG = 'assets/svg/geography.svg';
+  final String entertainmentSVG = 'assets/svg/entertainment.svg';
+  final String generalSVG = 'assets/svg/general.svg';
+  final String mathSVG = 'assets/svg/math.svg';
 }
