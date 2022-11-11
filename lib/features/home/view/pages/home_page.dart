69cf01fe03d35ceb9284e5943ff32324858dddc1
yasinkrcn <23yasin1999@gmail.com>
2022-11-11 13:29:10 +0300

    home page ui fix

--- lib/features/home/view/pages/home_page.dart
+++ lib/features/home/view/pages/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_quiz_app/core/constants/app_colors.dart';
 import 'package:flutter_firebase_quiz_app/core/constants/assets_paths.dart';
+import 'package:flutter_firebase_quiz_app/core/constants/category_list.dart';
 import 'package:flutter_firebase_quiz_app/core/init/injection_container.dart';
 import 'package:flutter_firebase_quiz_app/core/utils/route/route_manager.dart';
 import 'package:flutter_firebase_quiz_app/core/utils/route/router.dart';
 import 'package:flutter_firebase_quiz_app/features/auth/view_model/register_provider.dart';
+import 'package:flutter_firebase_quiz_app/features/quiz/view_model/question_controller.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
@@ -18,40 +20,45 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: AppColors.blackColor,
         actions: [
-
-          IconButton(onPressed: ()=> sl<RegisterProvider>().logout(), icon: const Icon(Icons.call_missed_outgoing_outlined))
+          IconButton(
+            onPressed: () => sl<RegisterProvider>().logout(),
+            icon: const Icon(Icons.call_missed_outgoing_outlined),
+          )
         ],
       ),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 0.8
-        ),
+            crossAxisCount: 2, childAspectRatio: 0.9),
         itemBuilder: ((context, index) => Padding(
               padding: const EdgeInsets.all(12),
               child: GestureDetector(
-                onTap: () => Go.to.page(PageRoutes.quizPage),
+                onTap: (() {
+                  sl<QuestionController>().categorySelect(index);
+                }),
                 child: Container(
-                 
                   decoration: BoxDecoration(
-                      color: Colors.red, borderRadius: BorderRadius.circular(8)),
+                      color: categoryList[index].categoryColor,
+                      borderRadius: BorderRadius.circular(24)),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       SvgPicture.asset(
-                        AssetsPath().loginSVG,
+                        categoryList[index].image,
                         height: 75,
                       ),
-                      const Text(
-                        'Category name',
-                        style: TextStyle(color: Colors.black, fontSize: 18),
+                      Text(
+                        categoryList[index].categoryName,
+                        style: TextStyle(
+                            color: categoryList[index].textColor,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500),
                       ),
                     ],
                   ),
                 ),
               ),
             )),
-        itemCount: 8,
+        itemCount: categoryList.length,
       ),
     );
   }
